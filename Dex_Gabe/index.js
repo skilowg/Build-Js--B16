b4939e138b73f6367991221235953709bb28977b
skilowg <skilowg@gmail.com>
2014-06-04 12:00:00 -0700

    Add more \'complex\' interaction, especially for daddy Dale

--- Dex_Gabe/index.js
+++ Dex_Gabe/index.js
@@ -1,6 +1,14 @@
 window.onload = function () {
   document.getElementById("prompt").onclick = function buttonClick() {
     var name = prompt("what is your name");
-    document.getElementById("message").innerHTML = name;
+    if (name == "dale" || name == "Dale" ) {
+      alert ("Sorry, Greedo shot first. Lol!");
+    }
+    else if (name.length > 5) {
+      alert ("Your name is too fucking long, change it!");
+    }
+    else {
+      alert ("Nice and to the point");
+    }
   };
 };
